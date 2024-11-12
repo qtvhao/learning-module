cd53931664c3559b5a3a544669f48ef4102bcac2
Your Name <you@example.com>
2024-11-12 09:26:32 +0100

    Add web middleware to learning routes for enhanced access control

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Qtvhao\LearningModule\Controllers\LearningController;
 
 Route::prefix('api/learning')
-    ->middleware('device.access')
+    ->middleware([
+        'web',
+        'device.access'
+    ])
     ->group(function () {
         Route::get('/articles', [LearningController::class, 'listArticles']);
         Route::get('/videos', [LearningController::class, 'listVideos']);
