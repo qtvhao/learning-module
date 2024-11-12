b7231c7c8ea871c55b686c3a17732dc33fce91fc
Your Name <you@example.com>
2024-11-12 06:48:02 +0100

    Add device access control dependency and middleware to learning routes

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Qtvhao\LearningModule\Controllers\LearningController;
 
-Route::prefix('api/learning')->group(function () {
-    Route::get('/articles', [LearningController::class, 'listArticles']);
-    Route::get('/videos', [LearningController::class, 'listVideos']);
-});
+Route::prefix('api/learning')
+    ->middleware('device.access')
+    ->group(function () {
+        Route::get('/articles', [LearningController::class, 'listArticles']);
+        Route::get('/videos', [LearningController::class, 'listVideos']);
+    });
